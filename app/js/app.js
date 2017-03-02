b316aa7602c88ff68fd1c71f9d092a4f55e3f750
ngie-mp <angeles.morap@gmail.com>
2017-03-02 17:49:38 +0100

    Add routing

--- app/js/app.js
+++ app/js/app.js
@@ -3,14 +3,26 @@ var app = angular.module('SwAppi', [
   'ngRoute'
  ]);
 
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+    templateUrl: 'pages/home.html',
+    controller: 'homeCtrl'
+    })
+    .when('/vehicles/', {
+      templateUrl: 'pages/vehicles.html',
+      controller: 'vechicleCtrl'
+    });
+});
+
 app.controller('homeCtrl', ['$scope', function($scope) {
-  $scope.saludo = 'Hola!';
+  $scope.titlePage = 'Welcome to starships gallery!';
 }]);
 
 app.controller('vechicleCtrl', ['$scope','$http', function($scope, $http) {
-
   $scope.vehicles = [];
-
+  $scope.isVisible = true;
+  $scope.titlePage = 'Starwars Starships';
   $scope.getVehicles = function() {
     $http({
     method: 'GET',
@@ -18,7 +30,9 @@ app.controller('vechicleCtrl', ['$scope','$http', function($scope, $http) {
     }).then(function successCallback(result) {
         console.log(result);
         $scope.vehicles = result.data.results;
+        $scope.isVisible = false;
         console.log($scope.vehicles);
+
     }, function errorCallback(result) {
       // called asynchronously if an error occurs
       console.log('no starships' + result);
