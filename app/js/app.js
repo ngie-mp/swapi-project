5fe3b579db4c9e13d191aa13e2c877e0dee96f91
ngie-mp <angeles.morap@gmail.com>
2017-03-02 14:56:41 +0100

    Add $http request to swapi
    also adding angular-material [wip]

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,22 @@
-var app = angular.module('SwAppi', []);
+var app = angular.module('SwAppi', ['ngMaterial']);
 
 app.controller('homeCtrl', ['$scope', function($scope) {
   $scope.saludo = 'Hola!';
+}]);
+
+app.controller('moviesCtrl', ['$scope','$http', function($scope, $http) {
+  $scope.movies = [];
+  $scope.getMovies = function() {
+    $http({
+    method: 'GET',
+    url: 'http://swapi.co/api/vehicles/'
+    }).then(function successCallback(result) {
+        console.log(result);
+        $scope.movies = result.data.results;
+        console.log($scope.movies);
+    }, function errorCallback(result) {
+      // called asynchronously if an error occurs
+      console.log('no movies' + result);
+    });
+  }
 }]);
